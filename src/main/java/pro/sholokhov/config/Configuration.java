7129cb52f82426d4f863f7e0c56d12970f7b29ca
Artem Sholokhov <sholokhov.aa@gmail.com>
2017-07-18 01:03:19 +0300

    Server configuration

--- src/main/java/pro/sholokhov/config/Configuration.java
+++ src/main/java/pro/sholokhov/config/Configuration.java
@@ -1,7 +1,61 @@
 package pro.sholokhov.config;
 
-public interface Configuration {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-  int port();
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.invoke.MethodHandles;
+import java.util.Properties;
+
+public class Configuration {
+
+  private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private static final String PROPERTIES_FILE = "app.properties";
+  private static Properties defaults = new Properties();
+
+  static {
+    defaults.setProperty(Names.PORT.val(), "5070");
+    defaults.setProperty(Names.ADDRESS.val(), "localhost");
+    defaults.setProperty(Names.API_PREFIX.val(), "api/v1");
+    defaults.setProperty(Names.MAX_CACHE_SIZE.val(), "10000");
+  }
+
+  public enum Names {
+    PORT("server.port"),
+    ADDRESS("server.address"),
+    API_PREFIX("api.prefix"),
+    MAX_CACHE_SIZE("cache.maxsize");
+
+    private String value;
+
+    Names(String value) {
+      this.value = value;
+    }
+
+    public String val() {
+      return value;
+    }
+
+  }
+
+  public static Properties loadConfig() {
+    return loadConfig(PROPERTIES_FILE);
+  }
+
+  public static Properties loadConfig(String file) {
+    Properties properties = new Properties(defaults);
+    try(InputStream stream = Configuration.class.getClassLoader().getResourceAsStream(file)) {
+      if (stream != null) {
+        properties.load(stream);
+      } else {
+        log.warn("Properties file {} was not loaded. Defaults were applied.", file);
+      }
+    } catch (IOException e) {
+      log.warn("Could not load properties from file {}. Defaults were applied.", file, e);
+    }
+    return properties;
+  }
 
 }
